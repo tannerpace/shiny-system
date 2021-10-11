c0f5530896d89821ea2f19577073bf2b2a8cc1a1
TannerB <tannerpace@gmail.com>
2021-10-11 16:04:11 -0400

    added assets and header / projects component

--- tbr1/src/App.js
+++ tbr1/src/App.js
@@ -1,11 +1,34 @@
 
 import './App.css';
+import { Typography } from '@mui/material';
+import Projects from './components/projects';
 
 function App() {
 
 
 
+  const Header = () => {
 
+
+    return (<nav id="navbar" class="nav">
+      <ul class="nav-list">
+        <li>
+          <div class="col-md-3 col-sm-3 col-xs-6"> <a href="#contact"
+            class="btn btn-sm animated-button victoria-one">Contact</a> </div>
+        </li>
+
+        <li>
+          <div class="col-md-3 col-sm-3 col-xs-6"> <a href="#projects"
+            class="btn btn-sm animated-button victoria-two">Projects</a> </div>
+        </li>
+
+        <li>
+          <div class="col-md-3 col-sm-3 col-xs-6"> <a href="#welcome-section"
+            class="btn btn-sm animated-button victoria-three">About</a> </div>
+        </li>
+      </ul>
+    </nav>)
+  }
 
 
 
@@ -17,8 +40,9 @@ function App() {
 
   return (
     <div className="App">
-      App works
-
+      <Typography>App works!</Typography>
+      <Header />
+      <Projects />
     </div>
   );
 }
---
+++ tbr1/src/components/projects/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <div>
+            <section id="projects" class="projects-section">
+                <h2 class="projects-section-header">These are some of my projects</h2>
+
+                <div class="projects-grid">
+                    <a href="https://tannerpace.github.io/crispy-couscous/" target="_blank" class="project project-tile">
+                        <img class="project-image" src="./assets/imgs/typer.png" alt="project" />
+                        <p class="project-title">
+                            <span class="code">&lt;</span>
+                            Typing Test
+                            <span class="code">&#47;&gt;</span>
+                        </p>
+                    </a>
+                    <a href="https://www.youtube.com/embed/HIsTgGAJVZ4" target="_blank" class="project project-tile">
+                        <img class="project-image" src="./assets/imgs/Screen Shot 2021-09-25 at 2.25.22 PM.png " alt="project" />
+                        <p class="project-title">
+                            <span class="code">&lt;</span>
+                            Parts Store
+                            <span class="code">&#47;&gt;</span>
+                        </p>
+                    </a>
+                    <a href="https://tannerpace.github.io/Todo-React/" target="_blank" class="project project-tile">
+                        <img class="project-image" src="./assets/imgs/todosmall.png" alt="project" />
+                        <p class="project-title">
+                            <span class="code">&lt;</span>
+                            Time To Todo App
+                            <span class="code">&#47;&gt;</span>
+                        </p>
+                    </a>
+                    <a href="https://singlepage.d1zqi00jmrlymq.amplifyapp.com/#page-top" target="_blank"
+                        class="project project-tile">
+                        <img class="project-image" src="./assets/imgs/air.png" alt="project" />
+                        <p class="project-title">
+                            <span class="code">&lt;</span>
+                            Airautonomo Business
+                            <span class="code">&#47;&gt;</span>
+                        </p>
+                    </a>
+                    <a href="./pages/shapley.html" target="_blank" class="project project-tile">
+                        <img class="project-image" src="./assets/imgs/shapely.png" alt="project" />
+                        <p class="project-title">
+                            <span class="code">&lt;</span>
+                            Tribute Page To Lloyd Shapley
+                            <span class="code">&#47;&gt;</span>
+                        </p>
+                    </a>
+
+                </div>
+
+            </section>
+        </div>
+    )
+}
