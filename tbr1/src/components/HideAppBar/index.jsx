79b1c2e973846f41902b5e8c090ee6ed9f201b23
TannerB <tannerpace@gmail.com>
2021-10-15 16:30:03 -0700

    cleans up imports fixes broken link

--- tbr1/src/components/HideAppBar/index.jsx
+++ tbr1/src/components/HideAppBar/index.jsx
@@ -8,18 +8,17 @@ import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Slide from '@mui/material/Slide';
-import { Link } from 'react-router-dom';
 
-import Nav from '../Nav';
+
 
 
 import {
-
+    Link,
     Switch,
     Route,
 
 } from "react-router-dom";
-import Welcome from '../Welcome'
+
 import Projects from '../Projects'
 import Resume from '../Resume'
 import Contact from '../Contact'
@@ -55,19 +54,13 @@ export default function HideAppBar(props) {
                 <AppBar>
                     <Toolbar >
 
-                        <Link smooth to="/shiny-system/welcome" class="btn btn-sm animated-button victoria-two">About</Link>
-
-
-
+                        <Link smooth to="/shiny-system/" class="btn btn-sm animated-button victoria-two">About</Link>
                         <Link smooth to="/shiny-system/projects" class="btn btn-sm animated-button victoria-one">Projects</Link>
-
-
                         <Link smooth to="/shiny-system/contact"
                             class="btn btn-sm animated-button victoria-two">Contact</Link>
-
                         <Link smooth to="/shiny-system/resume" class="btn btn-sm animated-button victoria-two">Resume</Link>
 
-                        {/* <Nav></Nav> */}
+
                     </Toolbar>
                 </AppBar>
             </HideOnScroll >
@@ -76,10 +69,7 @@ export default function HideAppBar(props) {
                 <Box sx={{ my: 2 }}>
                     <Switch>
                         <Route exact path="/shiny-system/">
-
                             <MyTyper />
-
-
                         </Route>
                         <Route path="/shiny-system/projects">
                             <Projects />
