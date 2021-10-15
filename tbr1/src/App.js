920dd9dea4064f21ca6ca4719a3535167f96cd95
TannerB <tannerpace@gmail.com>
2021-10-15 13:13:27 -0700

    HideAppbar holds most logic

--- tbr1/src/App.js
+++ tbr1/src/App.js
@@ -1,16 +1,6 @@
 import './App.css'
 import React, { useState } from 'react'
-import {
 
-  Switch,
-  Route,
-
-} from "react-router-dom";
-import Welcome from './Components/Welcome'
-import Projects from './Components/Projects'
-import Resume from './Components/Resume'
-import Contact from './Components/Contact'
-import Nav from './Components/Nav'
 import HideAppBar from './Components/HideAppBar';
 
 function App() {
@@ -19,20 +9,7 @@ function App() {
     <div className="App">
       <HideAppBar />
       {/* <Nav /> */}
-      <Switch>
-        <Route exact path="/shiny-system/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/shiny-system/projects">
-          <Projects />
-        </Route>
-        <Route path="/shiny-system/contact">
-          <Contact />
-        </Route >
-        <Route path="/shiny-system/resume">
-          <Resume />
-        </Route>
-      </Switch>
+
     </div >
   );
 }
