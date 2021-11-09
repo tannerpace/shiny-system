71e3175e03226cd10289b50b91360bc8538144e0
TannerB <tannerpace@gmail.com>
2021-11-09 16:19:42 -0500

    addsPhotoofme

--- tbr1/src/components/MyTyper/index.jsx
+++ tbr1/src/components/MyTyper/index.jsx
@@ -1,36 +1,41 @@
 import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
 import { Box } from '@mui/system'
+import PhotoOfMe from '../PhotoOfMe';
 const Welcome = [" Hello World!, my name is Tanner.\nI am a full stack web developer. I have experience with, Javascript\nTypescript\nAngular React MaterialUI \Express\nAxios\nmySQL\nMongoDB The list is always growing! "];
 const WelcomeSpeed = 30
-const LangSpeed = 10000
+
 
 
 
 const MyTyper = () => {
 
-    const handleType = (count) => {
-        // access word count number
+  const handleType = (count) => {
+    // access word count number
 
-    }
+  }
 
 
-    const { text } = useTypewriter({
-        words: Welcome, typeSpeed: WelcomeSpeed,
-    })
+  const { text } = useTypewriter({
+    words: Welcome, typeSpeed: WelcomeSpeed,
+  })
 
-    return (
-        <div className='typer '>
+  return (<>
+    <div className='typer photo__of__me'>
 
-            <div class="talk-bubble tri-right border round btm-left-in">
-                <div class="talktext">
-                    <p>
-                        {text}
-                    </p>
-                </div>
-            </div>
+      <div class="talk-bubble tri-right border round btm-left-in">
+        <div class="talktext">
+          <p>
+            {text}
+          </p>
 
         </div>
-    )
+
+      </div>
+
+    </div>
+
+  </>
+  )
 }
 
 
--- tbr1/src/components/contact/index.jsx
+++ tbr1/src/components/contact/index.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
 export default function Contact() {
-    return (
+  return (
 
-        <section id="contact" class="contact-section">
-            <div class="contact-section-header">
-                <h2>Get in touch to chat about a project!</h2>
-                <p className='chat'>Please leave me a voice mail</p>
-                <p className='chat'>Say hi!</p>
+    <section id="contact" class="contact-section">
+      <div class="contact-section-header">
+        <h2>Get in touch to chat about a project!</h2>
 
-            </div>
-            <div class="contact-links">
+        <p className='chat'>Lets get coffee !</p>
 
-                <a id="profile-link" href="https://github.com/tannerpace" target="_blank" class="btn contact-details" rel="noreferrer"><i
-                    class="fab fa-github"></i> GitHub</a>
+      </div>
+      <div class="contact-links">
 
-                <a href="mailto:tannerpace@gmail.com" class="btn contact-details"><i class="fas fa-at"></i> Send an
-                    email</a>
-                <a href="tel:8435139583" class="btn contact-details"><i class="fas fa-mobile-alt"></i> Call me</a>
-            </div>
-        </section>
+        <a id="profile-link" href="https://github.com/tannerpace" target="_blank" class="btn contact-details" rel="noreferrer"><i
+          class="fab fa-github"></i> GitHub</a>
 
-    )
+        <a href="mailto:tannerpace@gmail.com" class="btn contact-details"><i class="fas fa-at"></i> Send an
+          email</a>
+        <a href="tel:8435139583" class="btn contact-details"><i class="fas fa-mobile-alt"></i> Call me</a>
+      </div>
+    </section>
+
+  )
 }
 
