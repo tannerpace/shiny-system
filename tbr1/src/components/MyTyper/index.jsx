be11af089dda75f4e87acf3cd969680e0b8e57a5
TannerB <tannerpace@gmail.com>
2021-11-09 17:06:33 -0500

    fix shapley link move chat bubble arrow to right in

--- tbr1/src/components/MyTyper/index.jsx
+++ tbr1/src/components/MyTyper/index.jsx
@@ -1,7 +1,7 @@
 import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
 import { Box } from '@mui/system'
 import PhotoOfMe from '../PhotoOfMe';
-const Welcome = [" Hello World!, my name is Tanner.\nI am a full stack web developer. I have experience with, Javascript\nTypescript\nAngular React MaterialUI \Express\nAxios\nmySQL\nMongoDB The list is always growing! "];
+const Welcome = [" Hello World!,\n my name is Tanner Bleakley.\nI am a full stack web developer. I have experience with, Javascript\nTypescript\nAngular React MaterialUI \Express\nAxios\nmySQL\nMongoDB The list is always growing! "];
 const WelcomeSpeed = 30
 
 
@@ -22,7 +22,7 @@ const MyTyper = () => {
   return (<>
     <div className='typer photo__of__me'>
 
-      <div class="talk-bubble tri-right border round btm-left-in">
+      <div class="talk-bubble tri-right border round btm-right-in">
         <div class="talktext">
           <p>
             {text}
