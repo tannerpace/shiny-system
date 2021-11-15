12ed6ad8808142fbba49bbd96ec955fb3e069fc3
TannerB <tannerpace@gmail.com>
2021-11-15 00:52:27 -0500

    empty

--- tbr1/src/components/Resume/index.jsx
+++ tbr1/src/components/Resume/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ReactComponent as ResumeSvg } from "./resume.svg"
 import useStyles from "./styles"
-
+//
 
 export default function Resume() {
   const classes = useStyles()
